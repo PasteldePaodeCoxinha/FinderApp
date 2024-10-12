568316a9bf5eb09059b5ea160a698234fad61608
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2024-10-12 17:55:52 -0300

    Correção na checagem do match

--- FinderApp/src/pages/List.tsx
+++ FinderApp/src/pages/List.tsx
@@ -99,7 +99,7 @@ export default function List({ navigation }: Props) {
 
     async function checaMatch(usuario: Usuario) {
         const idUsuarioAtual = await AsyncStorage.getItem("idUsuario");
-        const response = await fetch(`https://finder-app-back.vercel.app/curtir/match?curtiu=${idUsuarioAtual}&curtido=${usuario.id}`);
+        const response = await fetch(`https://finder-app-back.vercel.app/curtir/match?curtiu=${usuario.id}&curtido=${idUsuarioAtual}`);
 
         console.log(await response.json());
 
