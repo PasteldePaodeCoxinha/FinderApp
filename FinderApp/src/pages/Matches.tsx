812b86436c0f5665dac0f05daf4f27323f19975c
Pastel Caique <caiquepericles.23@gmail.com>
2024-10-23 11:18:46 -0300

    Tirei os avisos dos matches e search

--- FinderApp/src/pages/Matches.tsx
+++ FinderApp/src/pages/Matches.tsx
@@ -1,21 +1,20 @@
-import { Button, Text, View } from "react-native";
-import Nav from "../components/Nav";
+import React from 'react';
+import { Button, Text, View } from 'react-native';
+import Nav from '../components/Nav';
 
 interface Props {
     navigation: any;
-};
+}
 
 export default function Matches({ navigation }: Props) {
     return (
         <View>
             <Text>Matches</Text>
-            <Nav
-                navigation={navigation}
-            ></Nav>
+            <Nav navigation={navigation}/>
             <Button
-                title="Login"
-                onPress={() => navigation.navigate("Landing")}
+            title="Login"
+                onPress={() => navigation.navigate('Landing')}
             />
         </View>
-    )
+    );
 }
